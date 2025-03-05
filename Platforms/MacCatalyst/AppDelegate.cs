a4c47db0fd5277f3f90b8594731b8d424ba3aeeb
Cesar-AMJ05 <cesaralbertomj@gmail.com>
2025-03-05 05:26:50 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Calculadora_PDM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
